3bce1f9cdf7642b1558252fea9e572215aefd429
Jay Lo <jaylo@kkstream.com>
2024-01-18 14:26:16 +0800

    Add a new project called BasicStreamIngesting

---
+++ BasicStreamIngesting/BasicStreamIngesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicStreamIngesting
+//
+//  Created by Tsung Cheng Lo on 2024/1/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
